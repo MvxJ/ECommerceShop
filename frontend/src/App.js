70fd437441738195738a194576b937e1ceda296f
mvxj <mvxj11@gmail.com>
2024-06-03 11:08:59 +0200

    Create product listing with fake products data, add Product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import { Container } from 'react-bootstrap';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div>
-        <Header />
-        <Container>
-            <main className="py-3">
-                <h1>Welcome!</h1>
-            </main>
-        </Container>
-        <Footer/>
-    </div>
+      <div>
+          <Header/>
+          <main className="py-3">
+              <Container>
+                  <HomeScreen />
+              </Container>
+          </main>
+    <Footer/>
+</div>
   );
 }
 
